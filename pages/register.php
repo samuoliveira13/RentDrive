be5a0126e4b756b7fffd40de4d131dc46c8f7855
up202108751feup <up202108751@edu.fe.up.pt>
2023-07-18 16:03:00 +0100

    Register Page starter

---
+++ pages/register.php
@@ -0,0 +1,16 @@
+<?php
+    declare(strict_types=1);
+    require_once(dirname(__DIR__).'/templates/common.tpl.php');
+    require_once(dirname(__DIR__).'/templates/auth.tpl.php');
+    require_once(dirname(__DIR__).'/classes/session.class.php');
+    $session = new Session();
+
+    $_SESSION['input']['f_name register'] = $_SESSION['input']['f_name register'] ?? "";
+    $_SESSION['input']['l_name register'] = $_SESSION['input']['l_name register'] ?? "";
+    $_SESSION['input']['email register'] = $_SESSION['input']['email register'] ?? "";
+    $_SESSION['input']['password register'] = $_SESSION['input']['password register'] ?? "";
+    $_SESSION['input']['confirm_password register'] = $_SESSION['input']['confirm_password register'] ?? "";
+
+    drawLogRegHeader();
+    drawRegisterForm();
+?>
